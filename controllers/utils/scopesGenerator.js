1788fc7d33bc7aca563bdbdb828febc4d2a7737b
CesarGP <cgpcsjcmp@gmail.com>
2021-01-26 13:01:23 +0100

    feat: validation and missing values function

--- controllers/utils/scopesGenerator.js
+++ controllers/utils/scopesGenerator.js
@@ -146,15 +146,80 @@ const getInfoYaml = (url, branch) => {
   });
 };
 
-/* const getMissingInfo = (infoObject) => {
-  const missingAtributes = "";
+const getMissingAndValidationInfo = (infoObject) => {
+  return new Promise((resolve, reject) => {
+    const missingAttributes = [];
+    const wrongAttributes = [];
 
-  axios.get("https://raw.githubusercontent.com/governify/audited-project-template/main/info.yml").then((response) => {
-    const originalInfoYaml = jsyaml.load(response.data).project;
-    if (!infoObject.name) {
+    axios.get("https://raw.githubusercontent.com/governify/audited-project-template/main/info.yml").then((response) => {
+      const originalInfoObject = jsyaml.load(response.data).project;
 
-    }
+      for (const key1 of Object.keys(originalInfoObject)) {
+        switch (key1) {
+          case 'members':
+          case 'identities':
+            if (infoObject[key1] === undefined) {
+              missingAttributes.push(key1);
+            } else {
+              for (const key2 of Object.keys(infoObject[key1])) {
+                for (const key3 of Object.keys(originalInfoObject[key1][key2])) {
+                  const missingAndValidation = checkField(infoObject[key1][key2][key3], originalInfoObject[key1][key2][key3], 'identities.' + key2 + '.' + key3);
+                  if (missingAndValidation === undefined) {
+                    missingAttributes.push('identities.' + key2 + '.' + key3)
+                  } else if (missingAndValidation !== null) {
+                    wrongAttributes.push(missingAndValidation)
+                  }
+                }
+              }
+            }
+            break;
+          default:
+            if (infoObject[key1] === undefined) {
+              missingAttributes.push(key1);
+            }
+        }
+      }
+      resolve({ missingAttributes: missingAttributes, wrongAttributes: wrongAttributes });
+    }).catch(err => {
+      reject(err);
+    });
   });
-} */
+}
+
+const checkField = (field, validationRule, fieldLocation) => {
+  if (field === undefined) {
+    return undefined;
+  } else {
+    switch (validationRule) {
+      case 'string':
+        if (field === '') {
+          return fieldLocation;
+        }
+        break;
+      case 'number':
+        var numberRegex = /^[0-9]+$/
+        if (!numberRegex.test(field)) {
+          return fieldLocation;
+        }
+        break;
+      case 'url':
+        var urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+        if (!urlRegex.test(field)) {
+          return fieldLocation;
+        }
+        break;
+      case 'email':
+        var emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+        if (!emailRegex.test(field)) {
+          return fieldLocation;
+        }
+        break;
+      default:
+        return 'unknownValidationRule(' + validationRule + ')-' + fieldLocation;
+    }
+  }
+  return null;
+}
+
 
 exports.generateFromGithubList = generateFromGithubList;
