ca95bd70fd6c0fc0fd2e39d641f1cb52bbc648f4
CesarGP <cgpcsjcmp@gmail.com>
2021-01-26 10:39:48 +0100

    fix: duplicated code

--- controllers/utils/scopesGenerator.js
+++ controllers/utils/scopesGenerator.js
@@ -4,6 +4,7 @@ const jsyaml = require('js-yaml');
 const githubRawUrl = 'https://raw.githubusercontent.com/';
 
 const infoFilename = 'info.test.yml';
+
 /*  generationRequest Example
 {
     "courseId": "cstest",
@@ -37,7 +38,7 @@ const generateFromGithubList = (generationRequest, substitute = false, branch = 
       const githubRepo = repoURL.split('github.com/')[1].split('/')[1];
 
       const promise = new Promise((resolve, reject) => {
-        axios.get(githubRawUrl + githubOwner + '/' + githubRepo + '/' + branch + '/' + infoFilename).then((response) => {
+        getInfoYaml(githubRawUrl + githubOwner + '/' + githubRepo + '/', branch).then((response) => {
           try {
             const infoJson = jsyaml.load(response.data).project;
 
@@ -113,87 +114,10 @@ const generateFromGithubList = (generationRequest, substitute = false, branch = 
           }
         }).catch(err => {
           console.log('Info.yml retrieval to principal branch failed. Retrying with master instead.', err.message);
-          // TODO - Recursive instead of duplicate. If fails it should try with master branch for older projects
-          axios.get(githubRawUrl + githubOwner + '/' + githubRepo + '/' + 'master' + '/' + infoFilename).then((response) => {
-            try {
-              const infoJson = jsyaml.load(response.data).project;
-
-              infoJson.projectId = base.development[0].classId + '-GH-' + githubOwner + '_' + githubRepo;
-
-              // Get Identities
-              const identities = [
-                {
-                  source: 'github',
-                  repository: githubRepo,
-                  repoOwner: githubOwner
-
-                }
-              ];
-
-              for (const identity of Object.keys(infoJson.identities)) {
-                const identityObject = { source: identity };
-
-                if (identity === 'pivotal') {
-                  identityObject.projectId = infoJson.identities[identity].projectId.toString();
-                } else if (identity === 'heroku') {
-                  identityObject.projectId = infoJson.identities[identity].appId;
-                }
-                identities.push(identityObject);
-              }
-              delete infoJson.identities; // Just for ordering the return object
-              infoJson.identities = identities;
-
-              // Get Members
-              const members = [];
-
-              for (const member of Object.keys(infoJson.members)) {
-                const memberObject = {
-                  memberId: infoJson.members[member].name.replace(' ', '') + '_' + infoJson.members[member].surname.replace(' ', '')
-                };
-
-                // Member identities
-                const memberIdentities = [];
-                if (infoJson.members[member].githubUsername) {
-                  memberIdentities.push({
-                    source: 'github',
-                    username: infoJson.members[member].githubUsername
-                  });
-                }
-                if (infoJson.members[member].pivotalUsername) {
-                  memberIdentities.push({
-                    source: 'pivotal',
-                    username: infoJson.members[member].pivotalUsername
-                  });
-                }
-                if (infoJson.members[member].herokuEmail) {
-                  memberIdentities.push({
-                    source: 'heroku',
-                    email: infoJson.members[member].herokuEmail
-                  });
-                }
-                memberObject.identities = memberIdentities;
-
-                // Credentials
-                memberObject.credentials = [];
-
-                members.push(memberObject);
-              }
-              delete infoJson.members; // Just for ordering the return object
-              infoJson.members = members;
-
-              // Push and resolve
-              projects.push(infoJson);
-              resolve();
-            } catch (err) {
-              console.log(err);
-              resolve();
-            }
-          }).catch(err => {
-            console.log(err);
-            resolve();
-          });
+          resolve();
         });
       });
+
 
       promises.push(promise);
     }
@@ -207,5 +131,31 @@ const generateFromGithubList = (generationRequest, substitute = false, branch = 
     });
   });
 };
+
+const getInfoYaml = (url, branch) => {
+  return new Promise((resolve, reject) => {
+    axios.get(url + branch + '/' + infoFilename).then((response) => {
+      resolve(response);
+    }).catch(err => {
+      if (branch !== "master") {
+        resolve(getInfoYaml(url, 'master'));
+      } else {
+        console.log(err)
+        resolve(undefined);
+      }
+    });
+  });
+}
+
+/*const getMissingInfo = (infoObject) => {
+  const missingAtributes = "";
+
+  axios.get("https://raw.githubusercontent.com/governify/audited-project-template/main/info.yml").then((response) => {
+    const originalInfoYaml = jsyaml.load(response.data).project;
+    if (!infoObject.name) {
+
+    }
+  });
+}*/
 
 exports.generateFromGithubList = generateFromGithubList;
