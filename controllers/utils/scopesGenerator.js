03e2df8ec04e1a45f04df8609154a20edca1bc1d
CesarGP <cgpcsjcmp@gmail.com>
2021-03-15 19:47:41 +0100

    fix: header

--- controllers/utils/scopesGenerator.js
+++ controllers/utils/scopesGenerator.js
@@ -429,8 +429,7 @@ const generateFromGithubList = (generationRequest) => {
 
 const getInfoYaml = (url, branch) => {
   return new Promise((resolve, reject) => {
-    console.log(url + branch + '/' + infoFilename, process.env.KEY_GITHUB ? 'token ' + process.env.KEY_GITHUB : '');
-    axios.get(url + branch + '/' + infoFilename, { headers: { Authentication: process.env.KEY_GITHUB ? 'token ' + process.env.KEY_GITHUB : '' } }).then((response) => {
+    axios.get(url + branch + '/' + infoFilename, { headers: { Authorization: process.env.KEY_GITHUB ? 'token ' + process.env.KEY_GITHUB : '' } }).then((response) => {
       resolve(response);
     }).catch(() => {
       if (branch !== 'master') {
