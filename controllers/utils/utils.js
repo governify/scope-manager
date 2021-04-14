66a0bf60f825468fc0a44bb101b4a0a73534d6a7
CesarGP <cgpcsjcmp@gmail.com>
2021-04-14 11:42:59 +0200

    feat: governify-commons requests and port changed

--- controllers/utils/utils.js
+++ controllers/utils/utils.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const fs = require('fs');
-const axios = require('axios');
 const mustache = require('mustache');
 mustache.escape = function (text) { return text; };
+const governify = require('governify-commons');
+
 const scopesGenerator = require('./scopesGenerator');
 
 let scopeObject;
@@ -34,7 +35,7 @@ const init = () => {
 const fetchScopes = () => {
   return new Promise((resolve, reject) => {
     console.log('Trying to fetch scopes.json file from assets.');
-    axios.get(process.env.URL_INT_ASSETS_MANAGER + '/api/v1/private/scope-manager/scopes.json', {
+    governify.infrastructure.getService('internal.assets').get('/api/v1/private/scope-manager/scopes.json', {
       params: {
         private_key: process.env.KEY_ASSETS_MANAGER_PRIVATE
       }
@@ -60,7 +61,7 @@ const fetchScopes = () => {
 const putScopes = () => {
   return new Promise((resolve, reject) => {
     console.log('Trying to PUT scopes.json file to assets.');
-    axios.put(process.env.URL_INT_ASSETS_MANAGER + '/api/v1/private/scope-manager/scopes.json', scopeObject, {
+    governify.infrastructure.getService('internal.assets').put('/api/v1/private/scope-manager/scopes.json', scopeObject, {
       params: {
         private_key: process.env.KEY_ASSETS_MANAGER_PRIVATE
       }
---
+++ server.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const deploy = (env) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const fs = require('fs');
+      const http = require('http');
+      const path = require('path');
+      require('dotenv').config();
+
+      const express = require('express');
+      const app = express();
+      const bodyParser = require('body-parser');
+      app.use(bodyParser.json({
+        strict: false
+      }));
+
+      const oasTools = require('oas-tools');
+      const jsyaml = require('js-yaml');
+      const serverPort = process.env.PORT || 5700;
+
+      const spec = fs.readFileSync(path.join(__dirname, '/api/oas-doc.yaml'), 'utf8');
+      const oasDoc = jsyaml.safeLoad(spec);
+
+      const optionsObject = {
+        controllers: path.join(__dirname, './controllers'),
+        loglevel: env === 'test' ? 'error' : 'info',
+        strict: false,
+        router: true,
+        validator: true
+      };
+
+      oasTools.configure(optionsObject);
+
+      oasTools.initialize(oasDoc, app, function () {
+        http.createServer(app).listen(serverPort, function () {
+          if (env !== 'test') {
+            console.log('App running at http://localhost:' + serverPort);
+            console.log('________________________________________________________________');
+            if (optionsObject.docs !== false) {
+              console.log('API docs (Swagger UI) available on http://localhost:' + serverPort + '/docs');
+              console.log('________________________________________________________________');
+            }
+          }
+        });
+      });
+
+      app.get('/info', function (req, res) {
+        res.send({
+          info: 'This API was generated using oas-generator!',
+          name: oasDoc.info.title
+        });
+      });
+
+      // quit on ctrl-c when running docker in terminal
+      process.on('SIGINT', function onSigint () {
+        console.log('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+        process.exit();
+      });
+
+      // quit properly on docker stop
+      process.on('SIGTERM', function onSigterm () {
+        console.log('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+        process.exit();
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+const undeploy = () => {
+  process.exit();
+};
+
+module.exports = {
+  deploy: deploy,
+  undeploy: undeploy
+};
